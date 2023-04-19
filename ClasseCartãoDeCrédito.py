8b83cc38c9b8b82518bbd082ab2e2522db5edc8a
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-04-19 17:54:58 -0300

    Update ClasseCartãoDeCrédito.py

--- ClasseCartãoDeCrédito.py
+++ ClasseCartãoDeCrédito.py
@@ -78,7 +78,19 @@ class cartao:
             print(f'Seu cartão ja está {self.__status}')
 
     def bloquear(self):
-        data_atual = (4, 2023)
+        data_atual = (4, 2023)if senha == self.__senha:
+104
+                    self.__limite_compra -= valor
+105
+                    self.__fatura_pagar += valor
+106
+                    self.__valor_min_pagar = self._fatura_pagar * 0.3
+107
+                    print('Compra Realizada.')
+108
+                else:
+109
+                    print('Senha inválida')
         if self.__status != 'bloqueado':
             if self.__validade[0] < data_atual[0] and self.__validade[1] >= data_atual[1]:
                 self.__status = 'bloqueado'
@@ -122,7 +134,19 @@ class cartao:
                 self.__fatura_pagar -= valor
                 self.__lim_compras += valor
                 print('Fatura paga.')
-                if self.__status == 'bloqueado':
+                if self.__statif senha == self.__senha:
+104
+                    self.__limite_compra -= valor
+105
+                    self.__fatura_pagar += valor
+106
+                    self.__valor_min_pagar = self._fatura_pagar * 0.3
+107
+                    print('Compra Realizada.')
+108
+                else:
+109
+                    print('Senha inválida')us == 'bloqueado':
                     self.desbloquear()
                 else:
                     pass
@@ -135,9 +159,24 @@ class cartao:
 ct= [cartao(1111, 'João Augusto', (7,2023), 567, 3000, 1234, status='desbloqueado'),
     cartao(1112, 'Matheus Henrique', (2, 2023), 550),
     cartao(1113, 'Maria Eduarda', (4,2023), 500, 4500, 1234),
-    cartao(1114, 'Eizabeth Webber', (10, 2023), 504),
+    cartao(1114, 'Eizabeth Webber', (10, 2023), 504), 
     cartao(1115, 'Elizandra Scott', (5, 2023), 405)]
 
 #O print do __str__
 for i in range(5):
     print(ct[i])
+   
+#Método bloquear
+print(ct[1].bloquear)
+print(ct[1].status)
+
+#Mudar senha
+print(ct[4].mudar_senha(345, 1234))
+print(ct[4].senha)
+
+#Comprar
+print(ct[3].comprar(1200,1234))
+#Pagar fatura
+print(ct[0].pagar_fatura(1050) 
+
+
