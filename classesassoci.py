07477c08e4156343157a5d051f945dc2b0f578c8
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-05-03 16:02:15 -0300

    Update classesassoci.py

--- classesassoci.py
+++ classesassoci.py
@@ -1,4 +1,4 @@
-class Pacinte:
+class Paciente:
     def __init__(self, id_paciente, nome, data_nasc, contato):
         self.__id_paciente = id_paciente
         self.nome = nome
@@ -6,7 +6,7 @@ class Pacinte:
         self.contato = contato
     
     def __str__(self):
-        return f'{self.__id_paciente}{self.nome}{self.data_nasc}{self.contato}'
+        return f'ID:{self.__id_paciente}\nNome:{self.nome}\nData de nascimento:{self.data_nasc}\nTelefone:{self.contato}'
 class Medico:
     def __init__(self, id_medico, crm, nome, especialidade):
         self.__id_medico = id_medico
@@ -14,14 +14,24 @@ class Medico:
         self.nome = nome
         self.especialidade = especialidade
     def __str__(self):
-        return f'{self.__id_medico}{self.crm}{self.nome}{self.especialidade}'
+        return f'ID:{self.__id_medico}\nCRM:{self.crm}\nNome:{self.nome}\nEspecialidade:{self.especialidade}'
 
 class Consultamedica:
-    def __init__(self, id_consulta, medico, paciente, data, data_retorno , pago=False):
+    def __init__(self, id_consulta, medico, paciente, data, data_retorno=None , pago=False):
         self.__id_consulta = id_consulta
-        self.medico = medico
-        self.paciente = paciente
+        if type(medico) == Medico:
+            self.__medico = medico
+        else:
+            raise 'Error!'
+        if type(paciente) == Paciente:
+            self.__paciente = paciente
+        else:
+            raise 'Error!'
         self.data = data
         self.data_retorno = data_retorno
         self.pago = pago
     
+    def __str__(self):
+        return f'Data da consulta:{self.data}\nPaciente:{self.__paciente.nome}\nMédico:{self.__medico.nome}'
+        
+
