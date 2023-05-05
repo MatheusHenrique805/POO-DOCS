50605fda085d98fcbf61c1d7072601a87160e246
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-05-05 16:40:44 -0300

    Update classesassoci.py

--- classesassoci.py
+++ classesassoci.py
@@ -1,21 +1,4 @@
-class Paciente:
-    def __init__(self, id_paciente, nome, data_nasc, contato):
-        self.__id_paciente = id_paciente
-        self.nome = nome
-        self.data_nasc = data_nasc
-        self.contato = contato
-    
-    def __str__(self):
-        return f'ID:{self.__id_paciente}\nNome:{self.nome}\nData de nascimento:{self.data_nasc}\nTelefone:{self.contato}'
-class Medico:
-    def __init__(self, id_medico, crm, nome, especialidade):
-        self.__id_medico = id_medico
-        self.crm = crm
-        self.nome = nome
-        self.especialidade = especialidade
-    def __str__(self):
-        return f'ID:{self.__id_medico}\nCRM:{self.crm}\nNome:{self.nome}\nEspecialidade:{self.especialidade}'
-
+from datetime import * 
 class Consultamedica:
     def __init__(self, id_consulta, medico, paciente, data, data_retorno=None , pago=False):
         self.__id_consulta = id_consulta
@@ -33,5 +16,21 @@ class Consultamedica:
     
     def __str__(self):
         return f'Data da consulta:{self.data}\nPaciente:{self.__paciente.nome}\nMédico:{self.__medico.nome}'
-        
+class Paciente:
+    def __init__(self, id_paciente, nome, data_nasc, contato):
+        self.__id_paciente = id_paciente
+        self.nome = nome
+        self.data_nasc = data_nasc
+        self.contato = contato
+    
+    def __str__(self):
+        return f'ID:{self.__id_paciente}\nNome:{self.nome}\nData de nascimento:{self.data_nasc}\nTelefone:{self.contato}'
+class Medico:
+    def __init__(self, id_medico, crm, nome, especialidade):
+        self.__id_medico = id_medico
+        self.crm = crm
+        self.nome = nome
+        self.especialidade = especialidade
+    def __str__(self):
+        return f'ID:{self.__id_medico}\nCRM:{self.crm}\nNome:{self.nome}\nEspecialidade:{self.especialidade}'
 
