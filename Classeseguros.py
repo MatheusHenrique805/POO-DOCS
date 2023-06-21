bc48398565c7062f53f0d8c4cbe2268156cf97ed
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-06-21 16:02:25 -0300

    Add files via upload

---
+++ Classeseguros.py
@@ -0,0 +1,32 @@
+class Cliente:
+    def __init__(self, cpf, nome, idade):
+        self.__cpf = cpf
+        self.__nome = nome
+        self.__idade = idade
+
+    @property
+    def cpf(self):
+        return self.__cpf
+    @property
+    def nome(self):
+        return self.__nome
+    @property
+    def idade(self):
+        return self.__idade
+    
+class Seguro:
+    def __init__(self, num_apolice, proprietario):
+        self._num_apolice = num_apolice
+        self._proprietario = proprietario
+    
+class SeguroVida(Seguro):
+    def __init__(self, num_apolice, proprietario, nome_beneficiario):
+        super().__init__(num_apolice, proprietario)
+        self._nome_beneficiario = nome_beneficiario
+class SeguroAutomovel(Seguro):
+    def __init__(self, num_apolice, proprietario, num_licenca, nome_modelo, ano, valor_auto):
+        super().__init__(num_apolice, proprietario)
+        self._num_licenca = num_licenca
+        self._nome_modelo = nome_modelo
+        self._ano = ano
+        self._valo_auto = valor_auto
